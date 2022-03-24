91bf776f1928a1af667224e0ed1fec2b23431511
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2022-03-23 23:51:44 -0500

    added some javascript but commented out its a rough draft

---
+++ js/config.js
@@ -0,0 +1,22 @@
+// make the console clear on every refresh
+console.clear();
+
+//------PASSWORD SLIDER PROPERTIES--------//
+
+// select the password length slider container which will effect the length property of the password
+//-- const slider = document.querySelector();
+
+// text which will be the value of the range slider
+//-- const sliderValue = document.querySelector();
+
+// use event listener to change value of text and fill slider
+//-- slider.querySelector( 'input' ).addEventListener( 'input', event => {
+//        sliderValue.setAttribute ( 'length', event.target.value);
+//          applyFill( event.target);
+// });
+
+// selecting range and passing it through a applyFill function
+// applyFill(slider.querySelector( 'input' ));
+
+// the applyFill FUNCTION
+// ??
