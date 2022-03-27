569c8d8df24d40f53b456b5c25732bded54ddeb6
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2022-03-26 23:32:56 -0500

    adding more functions from help from internet

--- js/config.js
+++ js/config.js
@@ -34,6 +34,19 @@ const randomLetter = {
 
 //------ Generator Functions --------//
 
-function getRandomLower () {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+// function getRandomLower () {
+//     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
+
+// function getRandomUpper() {
+//      return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// }
+
+// function getRandomNumber() {
+//      return String.fromCharCode(Math.floor(secureMathRandom() * 10 + 48);
+// }
+
+// function getRandomSymbol() {
+//     const symbols = '~!@#$%^&*()_+{}":?<>;.,';
+//     return symbols[Math.floor(Math.random() * symbols.length)];
+// }
